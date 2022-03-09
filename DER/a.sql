af7a9d4144cac80876f906c9867e83c1ef9590d7
Gustavo <gugul5@hotmail.com>
2022-03-09 17:27:15 -0300

    Lista de exercicios

---
+++ DER/a.sql
@@ -0,0 +1,4 @@
+use db_blogpessoal;
+
+insert into tb_postagem(data, texto, titulo) values (current_timestamp(), "Blog Pessoal funcionando!", "Minha primeira postagem");
+insert into tb_postagem(data, texto, titulo) values (current_timestamp(), "Blog Pessoal Consultando!", "Minha segunda postagem");
